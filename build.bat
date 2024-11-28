a139b8a31a9e2a221e49149503526ad31f18eeae
invalid <you@example.com>
2024-11-28 16:00:03 +0800

    11/28/2024 16:00:03:776

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 set RUST_BACKTRACE=1
 REM set PATH=%PATH%;"C:\Program Files\7-Zip"
 set RUSTFLAGS=-Zlocation-detail=none -Zfmt-debug=none -Ctarget-feature=+crt-static -Cdebuginfo=none
-cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size" --release
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size,panic_immediate_abort" --release
